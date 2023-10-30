ac827871eea003b777e0a9a6e25f2f3808f35ea8
Noam Ben Binyamin <101128914+Noam0@users.noreply.github.com>
2023-10-30 19:07:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+
+document.querySelector("#C").addEventListener("click" , playSoundC);
+
+function playSoundC(){
+    
+    var audioC = new Audio ("232002__danmitch3ll__xylophone-c.wav");
+    audioC.play();
+
+}
+
+
+document.querySelector("#D").addEventListener("click" , playSoundD);
+
+function playSoundD(){
+    
+    var audioD = new Audio ("232008__danmitch3ll__xylophone-d1.wav");
+    audioD.play();
+
+}
+
+document.querySelector("#E").addEventListener("click" , playSoundE);
+
+function playSoundE(){
+    
+    var audioE = new Audio ("232007__danmitch3ll__xylophone-e1.wav");
+    audioE.play();
+
+}
+
+
+document.querySelector("#F").addEventListener("click" , playSoundF);
+
+function playSoundF(){
+    
+    var audioF = new Audio ("232006__danmitch3ll__xylophone-f.wav");
+    audioF.play();
+
+}
+
+
+document.querySelector("#G").addEventListener("click" , playSoundG);
+
+function playSoundG(){
+    
+    var audioG = new Audio ("232005__danmitch3ll__xylophone-g.wav");
+    audioG.play();
+
+}
+
+document.querySelector("#A").addEventListener("click" , playSoundA);
+
+function playSoundA(){
+    
+    var audioA = new Audio ("232004__danmitch3ll__xylophone-a.wav");
+    audioA.play();
+
+}
+
+
+document.querySelector("#B").addEventListener("click" , playSoundB);
+
+function playSoundB(){
+    
+    var audioB = new Audio ("232003__danmitch3ll__xylophone-b.wav");
+    audioB.play();
+
+}
+
+
+document.querySelector("#C2").addEventListener("click" , playSoundC2);
+
+function playSoundC2(){
+    
+    var audioC2 = new Audio ("232001__danmitch3ll__xylophone-c2.wav");
+    audioC2.play();
+
+}
